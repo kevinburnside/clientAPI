dc4bb4319c6ad98ae5c36cfbccff864b96b56b67
kevinburnside <kevinburnside15@hotmail.com>
2018-01-16 21:58:01 -0500

    pushing code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,153 @@
-import { Component } from '@angular/core';
+import { Component, Oninit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Http, Response, Headers, RequestOptions, RequestOptionsArgs } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+
+} from '@angular/material';
+interface NewGroceryItem {
+  groceryName: string;
+  quantity: string;
+  id: any;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements Oninit {
   title = 'app';
+  public groceryLists = [
+    { GroceryListId: 'My Vegetarian', item: ['apples', 'carrots'] },
+    { GroceryListId: 'Cottage List' },
+    { GroceryListId: 'Kids Lunches', viewValue: 'Tacos' }
+  ];
+  public name;
+  public quantity;
+  public shareable: boolean;
+  public newGroceryItem: NewGroceryItem;
+  public groceries = ['apples', 'oranges', 'steak', 'fish', 'cheese'];
+  public selectedTab: string;
+  public groceryList: any;
+  public selectedGroceryList: any;
+  private url = '';
+  constructor(private http: Http, ) {
+    this.newGroceryItem = {
+      groceryName: 'as',
+      quantity: '1',
+      id: ''
+    };
+  }
+  ngOninit() {
+  this.groceryList = this.getGroceryAllLists();
+  
+  }
+
+  tabChanged(event) {
+    this.selectedTab = event.tab.textLabel;
+    console.log('Clicked: ' + event.tab.textLabel);
+    this.selectedGroceryList = this.getGroceryList(this.selectedTab);
+  }
+  deleteSelected(item) {
+    console.log(item);
+    this.deleteSelectedItem(item);
+  }
+  public addNewItem(newGroceryItem) {
+    this.newGroceryItem.groceryName = newGroceryItem.value;
+    const id = Math.random().toString;
+    console.log(this.newGroceryItem);
+    this.createNewItem(this.newGroceryItem, id);
+  }
+  public save() {
+    this.newGroceryItem.groceryName = this.name;
+    this.newGroceryItem.quantity = this.quantity;
+    console.log(this.newGroceryItem);
+    this.editItem(this.newGroceryItem, this.newGroceryItem.id);
+  }
+  public getGroceryList(userid: string) {
+    const args = new RequestOptions({ method: 'Get' });
+    return this.http.get(this.url + userid + '/', args)
+      .map((res) => res.json() as any)
+      .catch(this.handleErrorObservable)
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+  public getGroceryAllLists() {
+    const args = new RequestOptions({ method: 'Get' });
+    return this.http.get(this.url + '/', args)
+      .map((res) => res.json() as any)
+      .catch(this.handleErrorObservable)
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+  public createNewItem(item: any, id: any) {
+    return this.http.post(this.url + id, item)
+        .map(this.extractData)
+        .catch(this.handleErrorObservable)
+        .subscribe((data) => {
+          console.log(data);
+        });
 }
+public editItem(item: any, id: any) {
+  return this.http.put(this.url + id, item)
+      .map(this.extractData)
+      .catch(this.handleErrorObservable)
+      .subscribe((data) => {
+        console.log(data);
+      });
+}
+public deleteSelectedItem(id: string): Observable<any> {
+  return this.http.delete(this.url, id)
+      .map(this.extractData)
+      .catch(this.handleErrorObservable);
+}
+  private extractData(res: Response) {
+    const body = res.json();
+    console.log(body);
+    return body;
+  }
+
+  private handleErrorObservable(error: Response | any) {
+    console.error(error.message || error);
+    return Observable.throw(error.message || error);
+  }
+
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,66 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
 
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatListModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
